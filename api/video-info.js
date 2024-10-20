e99eb08b1e60fd469a1f9eadced630a60932fa40
JustNoNameEhem <123246586+JustNoNameEhem@users.noreply.github.com>
2024-10-20 13:26:54 +0800

    Update video-info.js

--- api/video-info.js
+++ api/video-info.js
@@ -4,6 +4,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Origin', 'https://bahrananime.blogspot.com');
 
     // Handle preflight OPTIONS request for CORS
     if (req.method === 'OPTIONS') {
